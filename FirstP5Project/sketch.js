0b089e47a74a54571648bebb8cbe5fa87f95d525
aliwmortazavi <34728848+aliwmortazavi@users.noreply.github.com>
2017-12-21 00:55:12 -0500

    Update sketch.js

--- FirstP5Project/sketch.js
+++ FirstP5Project/sketch.js
@@ -20,7 +20,7 @@ function draw() {
     stroke(r, g, b, 50);
     strokeWeight(4);
     fill(r, g, b, 100);
-    ellipse(width/2, height/2, 150, 150);
+    ellipse(window.innerWidth/2, window.innerHeight/2, 150, 150);
     console.log("draw funciton");
 }
 
